418e92d756c0fd9ac381d92a77e6dd1634dc43a6
Introdructor <yashkachave7@gmail.com>
2020-12-05 10:50:42 +0530

    trinket-common:fix some issues

--- trinket.mk
+++ trinket.mk
@@ -75,8 +75,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0.vendor \
-    libdng_sdk.vendor \
-    GCamGo
+    libdng_sdk.vendor
 
 # CNE
 PRODUCT_PACKAGES += \
